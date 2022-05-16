9a14115674bf05dde23e1511ca89c4c8649f3df9
Kirill Katkov <k.katkov@dodopizza.com>
2022-05-16 09:18:30 +0300

    update test

--- src/test/java/tests/demoqa/AstrioPageTests.java
+++ src/test/java/tests/demoqa/AstrioPageTests.java
@@ -32,222 +32,222 @@ public class AstrioPageTests extends TestBase{
 //        Selenide.closeWebDriver();
 //    }
 
+//    @Test
+//    @Tag("astrio")
+//    void openPageTest() {
+//        astrioPage.openPage();
+//
+//        astrioPage.checkCopyright();
+//
+//        astrioPage.checkResultMain("Вакансии")
+//                .checkResultMain("О нас")
+//                .checkResultMain("Клиенты")
+//                .checkResultMain("Услуги")
+//                .checkResultMain("Блог")
+//                .checkResultMain("Контакты");
+//
+//        astrioPage.openNavLinkVacancies();
+//
+//        astrioPage.checkResultVacancies("О нас")
+//                .checkResultVacancies("Что мы делаем")
+//                .checkResultVacancies("Обучение в ASTRIO")
+//                .checkResultVacancies("Ваша карьера")
+//                .checkResultVacancies("Стажировка и практика")
+//                .checkResultVacancies("Наши вакансии");
+//
+//
+//        astrioPage.openScrollNavAboutUS()
+//                .aboutUCheckTitle("Работа в одной из лучших IT компаний Ульяновска")
+//                .aboutUCheckText("Высокий уровень проектов,");
+//
+//
+//        //astrioPage.openMenuLinkAboutUs();
+//        astrioPage.openMenuLinkDoing()
+//                .DoingCheckTitle("Наши проекты")
+//                .DoingUCheckText("Наша основная платформа - Magento");
+//
+//        astrioPage.openMenuLinkEducation()
+//                .EducationCheckTitle("Отлаженный процесс обучения")
+//                .EducationUCheckText("Наш процесс постоянно шлифуется последние 6 лет.");
+//
+//        astrioPage.openMenuLinkCareer();
+//        astrioPage.setContactName("Ruslan")
+//                .setContactEmail("1ya-ruslan@gmail.com")
+//                .setContactPhone("790429445322");
+//        //.careerButton()
+//        //.careerCheckForm("Ваше сообщение получено и мы ответим на него в ближайшее время. Спасибо за сообщение.");
+//
+//        astrioPage
+//                .setVacanciesEmail("1ya-ruslan@gmail.com")
+//                .setVacanciesPhone("790429445322")
+//                .setVacanciesComment("Ruslan");
+////                .vacanciesButton()
+////                .vacanciesCheckForm("Ваше сообщение получено и мы ответим на него в ближайшее время. Спасибо за сообщение.");
+//
+//
+//        //$x("(//div[@role='tablist']/div)[2]").click();
+//
+//
+//        //$$("//div[@class='careerBlock']//p)").shouldHave(itemWithText("Разработка ведется на современных платформах, с использованием самых современных инструментов разработки. В работе активно используются следующие технологии и инструменты: паттерны проектирования / MVC, PWA, phpStorm, Git, Gitlab CI, Newrelic, Elasticsearch, GraphQL, Docker."));
+//
+//    }
+
     @Test
     @Tag("astrio")
-    void openPageTest() {
-        astrioPage.openPage();
-
-        astrioPage.checkCopyright();
-
-        astrioPage.checkResultMain("Вакансии")
-                .checkResultMain("О нас")
-                .checkResultMain("Клиенты")
-                .checkResultMain("Услуги")
-                .checkResultMain("Блог")
-                .checkResultMain("Контакты");
-
-        astrioPage.openNavLinkVacancies();
-
-        astrioPage.checkResultVacancies("О нас")
-                .checkResultVacancies("Что мы делаем")
-                .checkResultVacancies("Обучение в ASTRIO")
-                .checkResultVacancies("Ваша карьера")
-                .checkResultVacancies("Стажировка и практика")
-                .checkResultVacancies("Наши вакансии");
-
-
-        astrioPage.openScrollNavAboutUS()
-                .aboutUCheckTitle("Работа в одной из лучших IT компаний Ульяновска")
-                .aboutUCheckText("Высокий уровень проектов,");
-
-
-        //astrioPage.openMenuLinkAboutUs();
-        astrioPage.openMenuLinkDoing()
-                .DoingCheckTitle("Наши проекты")
-                .DoingUCheckText("Наша основная платформа - Magento");
-
-        astrioPage.openMenuLinkEducation()
-                .EducationCheckTitle("Отлаженный процесс обучения")
-                .EducationUCheckText("Наш процесс постоянно шлифуется последние 6 лет.");
-
-        astrioPage.openMenuLinkCareer();
-        astrioPage.setContactName("Ruslan")
-                .setContactEmail("1ya-ruslan@gmail.com")
-                .setContactPhone("790429445322");
-        //.careerButton()
-        //.careerCheckForm("Ваше сообщение получено и мы ответим на него в ближайшее время. Спасибо за сообщение.");
-
-        astrioPage
-                .setVacanciesEmail("1ya-ruslan@gmail.com")
-                .setVacanciesPhone("790429445322")
-                .setVacanciesComment("Ruslan");
-//                .vacanciesButton()
-//                .vacanciesCheckForm("Ваше сообщение получено и мы ответим на него в ближайшее время. Спасибо за сообщение.");
-
-
-        //$x("(//div[@role='tablist']/div)[2]").click();
-
-
-        //$$("//div[@class='careerBlock']//p)").shouldHave(itemWithText("Разработка ведется на современных платформах, с использованием самых современных инструментов разработки. В работе активно используются следующие технологии и инструменты: паттерны проектирования / MVC, PWA, phpStorm, Git, Gitlab CI, Newrelic, Elasticsearch, GraphQL, Docker."));
+    @DisplayName("Проверка открытия страницы и поиск копирайта на главной странице")
+    void checkCopyrightTest() {
+        step("Открытие страницы - https://astrio.ru/", () -> {
+            astrioPage.openPage();
+        });
+        step("Проверка копирайта - astrio", () -> {
+            astrioPage.checkCopyright();
+        });
 
     }
 
-//    @Test
-//    @Tag("astrio")
-//    @DisplayName("Проверка открытия страницы и поиск копирайта на главной странице")
-//    void checkCopyrightTest() {
-//        step("Открытие страницы - https://astrio.ru/", () -> {
-//            astrioPage.openPage();
-//        });
-//        step("Проверка копирайта - astrio", () -> {
-//            astrioPage.checkCopyright();
-//        });
-//
-//    }
-//
-//    @Test
-//    @Tag("astrio")
-//    @DisplayName("Проверка меню на главной страницы")
-//    void checkMainMenu() {
-//        step("Открытие страницы - https://astrio.ru/", () -> {
-//            astrioPage.openPage();
-//        });
-//
-//        step("Проверка присутствия на странице пунктов меню - Вакансия, О нас, Клиенты, Услуги, Блог, Контакты", () -> {
-//            astrioPage.checkResultMain("Вакансии")
-//                    .checkResultMain("О нас")
-//                    .checkResultMain("Клиенты")
-//                    .checkResultMain("Услуги")
-//                    .checkResultMain("Блог")
-//                    .checkResultMain("Контакты");
-//        });
-//    }
-//
-//    @Test
-//    @Tag("astrio")
-//    @DisplayName("Открытие страницы вакансий и проверка меню")
-//    void vacanciesOpenAndCheck() {
-//        step("Открытие страницы - https://astrio.ru/", () -> {
-//            astrioPage.openPage();
-//        });
-//        step("Открытие страницы Вакансий", () -> {
-//            astrioPage.openNavLinkVacancies();
-//        });
-//
-//        step("Проверка присутствия на странице пунктов меню - О нас, Что мы делаем, Обучение в Astrio, Ваша курьера, Стажировка и практика, Наши вакансии", () -> {
-//            astrioPage.checkResultVacancies("О нас")
-//                    .checkResultVacancies("Что мы делаем")
-//                    .checkResultVacancies("Обучение в ASTRIO")
-//                    .checkResultVacancies("Ваша карьера")
-//                    .checkResultVacancies("Стажировка и практика")
-//                    .checkResultVacancies("Наши вакансии");
-//        });
-//
-//    }
-//
-//    @Test
-//    @Tag("astrio")
-//    @DisplayName("Переход к пунту меню - О нас и проверка контента")
-//    void aboutUSOpenAndCheck() {
-//        step("Открытие страницы - https://astrio.ru/", () -> {
-//            astrioPage.openPage();
-//        });
-//        step("Открытие страницы Вакансий", () -> {
-//            astrioPage.openNavLinkVacancies();
-//        });
-//
-//        step("Переход к пункту меню о Нас", () -> {
-//            astrioPage.openScrollNavAboutUS();
-//        });
-//
-//        step("Проверка текста заголовка", () -> {
-//            astrioPage.aboutUCheckTitle("Работа в одной из лучших IT компаний Ульяновска");
-//        });
-//
-//        step("Проверка текста параграфа", () -> {
-//            astrioPage.aboutUCheckText("Высокий уровень проектов,");
-//        });
-//    }
-//
-//    @Test
-//    @Tag("astrio")
-//    @DisplayName("Переход к пунту меню - Что мы делаем и проверка контакета")
-//    void linkDoingOpenAndCheck() {
-//        step("Открытие страницы - https://astrio.ru/", () -> {
-//            astrioPage.openPage();
-//        });
-//        step("Открытие страницы Вакансий", () -> {
-//            astrioPage.openNavLinkVacancies();
-//        });
-//        step("Переход к пункту меню - Что мы делаем", () -> {
-//            astrioPage.openMenuLinkDoing();
-//        });
-//
-//        step("Проверка текста заголовка", () -> {
-//            astrioPage.DoingCheckTitle("Наши проекты");
-//        });
-//
-//        step("Проверка текста параграфа", () -> {
-//            astrioPage.DoingUCheckText("Наша основная платформа - Magento");
-//        });
-//    }
-//
-//
-//    @Test
-//    @Tag("astrio")
-//    @DisplayName("Переход к пунту меню - Обучение и карьера и проверка контекста")
-//    void linkEducationOpenAndCheck() {
-//        step("Открытие страницы - https://astrio.ru/", () -> {
-//            astrioPage.openPage();
-//        });
-//        step("Открытие страницы Вакансий", () -> {
-//            astrioPage.openNavLinkVacancies();
-//        });
-//        step("Переход к пункту меню - Обучение и карьера", () -> {
-//            astrioPage.openMenuLinkEducation();
-//        });
-//
-//        step("Проверка текста заголовка", () -> {
-//            astrioPage.EducationCheckTitle("Отлаженный процесс обучения");
-//        });
-//
-//        step("Проверка текста параграфа", () -> {
-//            astrioPage.EducationUCheckText("Наш процесс постоянно шлифуется последние 6 лет.");
-//        });
-//    }
-//
-//    @Test
-//    @Tag("astrio")
-//    @DisplayName("Заполнение формы для Старта в IT проверка отправки")
-//    void setAndCheckLinkCareer() {
-//        step("Открытие страницы - https://astrio.ru/", () -> {
-//            astrioPage.openPage();
-//        });
-//        step("Открытие страницы Вакансий", () -> {
-//            astrioPage.openNavLinkVacancies();
-//        });
-//        step("Переход к пункту меню - Ваша карьера", () -> {
-//            astrioPage.openMenuLinkCareer();
-//        });
-//
-//        step("Заполнение имени в форме", () -> {
-//            astrioPage.setContactName("Ruslan");
-//        });
-//        step("Заполнение емайла  в форме", () -> {
-//            astrioPage.setContactEmail("1ya-ruslan@gmail.com");
-//        });
-//        step("Заполнение телефона  в форме", () -> {
-//            astrioPage.setContactPhone("790429445322");
-//        });
-//
-//        step("Клик по кнопке - отправка формы", () -> {
-//            astrioPage.careerButton();
-//        });
-//
-//        step("Проверка заполнения формы", () -> {
-//            astrioPage.careerCheckForm("Ваше сообщение получено и мы ответим на него в ближайшее время. Спасибо за сообщение.");
-//        });
-//    }
+    @Test
+    @Tag("astrio")
+    @DisplayName("Проверка меню на главной страницы")
+    void checkMainMenu() {
+        step("Открытие страницы - https://astrio.ru/", () -> {
+            astrioPage.openPage();
+        });
+
+        step("Проверка присутствия на странице пунктов меню - Вакансия, О нас, Клиенты, Услуги, Блог, Контакты", () -> {
+            astrioPage.checkResultMain("Вакансии")
+                    .checkResultMain("О нас")
+                    .checkResultMain("Клиенты")
+                    .checkResultMain("Услуги")
+                    .checkResultMain("Блог")
+                    .checkResultMain("Контакты");
+        });
+    }
+
+    @Test
+    @Tag("astrio")
+    @DisplayName("Открытие страницы вакансий и проверка меню")
+    void vacanciesOpenAndCheck() {
+        step("Открытие страницы - https://astrio.ru/", () -> {
+            astrioPage.openPage();
+        });
+        step("Открытие страницы Вакансий", () -> {
+            astrioPage.openNavLinkVacancies();
+        });
+
+        step("Проверка присутствия на странице пунктов меню - О нас, Что мы делаем, Обучение в Astrio, Ваша курьера, Стажировка и практика, Наши вакансии", () -> {
+            astrioPage.checkResultVacancies("О нас")
+                    .checkResultVacancies("Что мы делаем")
+                    .checkResultVacancies("Обучение в ASTRIO")
+                    .checkResultVacancies("Ваша карьера")
+                    .checkResultVacancies("Стажировка и практика")
+                    .checkResultVacancies("Наши вакансии");
+        });
+
+    }
+
+    @Test
+    @Tag("astrio")
+    @DisplayName("Переход к пунту меню - О нас и проверка контента")
+    void aboutUSOpenAndCheck() {
+        step("Открытие страницы - https://astrio.ru/", () -> {
+            astrioPage.openPage();
+        });
+        step("Открытие страницы Вакансий", () -> {
+            astrioPage.openNavLinkVacancies();
+        });
+
+        step("Переход к пункту меню о Нас", () -> {
+            astrioPage.openScrollNavAboutUS();
+        });
+
+        step("Проверка текста заголовка", () -> {
+            astrioPage.aboutUCheckTitle("Работа в одной из лучших IT компаний Ульяновска");
+        });
+
+        step("Проверка текста параграфа", () -> {
+            astrioPage.aboutUCheckText("Высокий уровень проектов,");
+        });
+    }
+
+    @Test
+    @Tag("astrio")
+    @DisplayName("Переход к пунту меню - Что мы делаем и проверка контакета")
+    void linkDoingOpenAndCheck() {
+        step("Открытие страницы - https://astrio.ru/", () -> {
+            astrioPage.openPage();
+        });
+        step("Открытие страницы Вакансий", () -> {
+            astrioPage.openNavLinkVacancies();
+        });
+        step("Переход к пункту меню - Что мы делаем", () -> {
+            astrioPage.openMenuLinkDoing();
+        });
+
+        step("Проверка текста заголовка", () -> {
+            astrioPage.DoingCheckTitle("Наши проекты");
+        });
+
+        step("Проверка текста параграфа", () -> {
+            astrioPage.DoingUCheckText("Наша основная платформа - Magento");
+        });
+    }
+
+
+    @Test
+    @Tag("astrio")
+    @DisplayName("Переход к пунту меню - Обучение и карьера и проверка контекста")
+    void linkEducationOpenAndCheck() {
+        step("Открытие страницы - https://astrio.ru/", () -> {
+            astrioPage.openPage();
+        });
+        step("Открытие страницы Вакансий", () -> {
+            astrioPage.openNavLinkVacancies();
+        });
+        step("Переход к пункту меню - Обучение и карьера", () -> {
+            astrioPage.openMenuLinkEducation();
+        });
+
+        step("Проверка текста заголовка", () -> {
+            astrioPage.EducationCheckTitle("Отлаженный процесс обучения");
+        });
+
+        step("Проверка текста параграфа", () -> {
+            astrioPage.EducationUCheckText("Наш процесс постоянно шлифуется последние 6 лет.");
+        });
+    }
+
+    @Test
+    @Tag("astrio")
+    @DisplayName("Заполнение формы для Старта в IT проверка отправки")
+    void setAndCheckLinkCareer() {
+        step("Открытие страницы - https://astrio.ru/", () -> {
+            astrioPage.openPage();
+        });
+        step("Открытие страницы Вакансий", () -> {
+            astrioPage.openNavLinkVacancies();
+        });
+        step("Переход к пункту меню - Ваша карьера", () -> {
+            astrioPage.openMenuLinkCareer();
+        });
+
+        step("Заполнение имени в форме", () -> {
+            astrioPage.setContactName("Ruslan");
+        });
+        step("Заполнение емайла  в форме", () -> {
+            astrioPage.setContactEmail("1ya-ruslan@gmail.com");
+        });
+        step("Заполнение телефона  в форме", () -> {
+            astrioPage.setContactPhone("790429445322");
+        });
+
+        step("Клик по кнопке - отправка формы", () -> {
+            astrioPage.careerButton();
+        });
+
+        step("Проверка заполнения формы", () -> {
+            astrioPage.careerCheckForm("Ваше сообщение получено и мы ответим на него в ближайшее время. Спасибо за сообщение.");
+        });
+    }
 
 
 
